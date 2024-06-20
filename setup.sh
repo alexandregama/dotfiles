4894d3780f55ac9dcd5c5c816ab94353d5dc1815
Alexandre Gama <alexandre.gama.lima@gmail.com>
2024-06-20 10:48:27 -0300

    Adds the Alacritty config folder

--- setup.sh
+++ setup.sh
@@ -31,4 +31,12 @@ if [ -d "$NVIM_CONFIG" ]; then
 fi
 ln -sf $DIR/nvim ~/.config/nvim
 
+# Remove existing Alacritty config folder it exists and create symlink
+ALACRITTY_CONFIG="$HOME/.config/alacritty"
+if [ -d "$ALACRITTY_CONFIG" ]; then
+  rm -rf "$ALACRITTY_CONFIG"
+  echo "Removed existing Alacritty config folder"
+fi
+ln -sf $DIR/alacritty ~/.config/alacritty
+
 echo "Symbolic links have been created successfully."
